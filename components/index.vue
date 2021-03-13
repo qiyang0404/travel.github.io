9fb0ea0a880f80b20880aa29c04cf48fff45f2a6
qiyang <876429705@qq.com>
2021-03-13 11:53:29 +0800

    Signed-off-by:qiyang <876429705@qq.com>

---
+++ components/index.vue
@@ -0,0 +1,48 @@
+<template>
+    <!--
+      这里的dom节点你可以使用你熟悉的element-ui中的组件代替，
+      由于我这个demo小项目用的是vant不是element这里就直接用原生的dom
+      自己在使用的时候可以稍作改动
+    -->
+    <div class="list">
+        <div @click.prevent="handleClick">
+            <a :href="model.linkUrl">{{ model.name }}</a>
+        </div>
+        <div v-show="reveal" v-if="isDispaly" style="margin-left:20px;">
+            <list-menu v-for="item in model.children" :key="item.id" :model="item" />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+	name: "listMenu",
+    props: ["model"],
+    components: {
+    },
+    data () {
+	    return {
+		    reveal: false
+	    };
+    },
+
+    mounted() {
+    },
+
+    methods: {
+	    handleClick() {
+		    if (this.isDispaly) {
+			    this.reveal = !this.reveal;
+		    }
+	    }
+    },
+
+    computed: {
+	    isDispaly() {
+		    return this.model.children && this.model.children.length;
+	    }
+    },
+}
+</script>
+<style lang="scss" scoped>
+</style>
